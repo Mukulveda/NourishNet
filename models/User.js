543c2de8795ae511dc1c1da17ef12afb45858b9c
mukjain33 <juug22btech56592@gmail.com>
2025-04-10 14:47:02 +0530

    Email fix

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const passportLocalMongoose = require("passport-local-mongoose");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   username: String,
   email: {
     type: String,
