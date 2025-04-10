acd2fd448aca77277f035f66cbaa1880e3e3759d
mukjain33 <juug22btech56592@gmail.com>
2025-04-10 14:45:41 +0530

    Email validation

--- models/User.js
+++ models/User.js
@@ -2,13 +2,15 @@ const passportLocalMongoose = require("passport-local-mongoose");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
+const userSchema = new mongoose.Schema({
+  username: String,
   email: {
     type: String,
     required: true,
-    unique: true, // Prevents duplicate email registrations
-  },
+    match: [/^[a-zA-Z0-9._%+-]+@gmail\.com$/, 'Please enter a valid Gmail address']
+  }
 });
+
 
 // Adds username, hash, salt, and helper methods (authenticate, register, etc.)
 UserSchema.plugin(passportLocalMongoose);
